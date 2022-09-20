bafd1c71fd369a4cd2a444a20af4a5f0243d3319
aleksi <aleksi.latukka@gmail.com>
2022-09-21 00:44:25 +0300

    palautus

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
 print("Hello World!")
-=======
-print("Hello world!")
-print("Hello from feature")
-<<<<<<< HEAD
->>>>>>> feature/create-awesome
-=======
-print("Hello from release")
->>>>>>> release
